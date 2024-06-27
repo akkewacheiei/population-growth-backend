e51c8300b8e3c8c747fb1a02cf6f593f8c7202e2
Broy <akkewach.yodsomboon@gmail.com>
2024-06-28 00:12:43 +0700

    test fixed bug

--- api/index.js
+++ api/index.js
@@ -411,14 +411,7 @@ app.get("/", (req, res) => {
 app.get("/population", (req, res) => {
   const results = {};
 
-/*   const path = require('./api/population-and-demography.csv');
-
-  const coolPath = path.join(__dirname, './api/population-and-demography.csv');
- */
-
-
-
-  fs.createReadStream("__dirname + '/api/population-and-demography.csv")
+  fs.createReadStream(__dirname + "/population-and-demography.csv")
     .pipe(csv())
     .on("data", (data) => {
       const year = data.Year;
